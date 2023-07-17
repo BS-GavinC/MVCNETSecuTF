930f5535ac8b8478ef1b97ce07b26999aef42115
VZGLY <113683718+BS-GavinC@users.noreply.github.com>
2023-07-17 11:28:16 +0200

    Initial commit

---
+++ MVCNETSecuTF/Controllers/CarController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCNETSecuTF.Models.Forms;
+
+namespace MVCNETSecuTF.Controllers;
+
+public class CarController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(CreateCarForm form)
+    {
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("index");
+        }
+        Console.WriteLine(ModelState.IsValid);
+        foreach (var Model in ModelState)
+        {
+            foreach (var error in Model.Value.Errors)
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+        }
+        
+        return View(form);
+    }
+    
+}
---
+++ MVCNETSecuTF/Models/Forms/CreateCarForm.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCNETSecuTF.Models.Forms;
+
+public class CreateCarForm
+{
+    
+    [Required(ErrorMessage = "Le Champ Marque est requis je te le dis !")]
+    [MinLength(3, ErrorMessage = "La Longueur Minimum de Marque est de 3")]
+    [MaxLength(25, ErrorMessage = "La Longueur Maximum de Marque est de 25")]
+    public string Marque { get; set; }
+    
+    
+    [Required]
+    [MinLength(3)]
+    [MaxLength(25)]
+    public string Modele { get; set; }
+    
+    
+    [Required]
+    [Range(1900, 2023)]
+    public int Annee { get; set; }
+    
+    [MinLength(3)]
+    [MaxLength(25)]
+    public string? Couleur { get; set; }
+    
+    [Required]
+    [Range(1, 7)]
+    public int Portes { get; set; }
+}
